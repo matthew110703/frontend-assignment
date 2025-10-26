b5ca84b595fbe102c2a595470e0a2d2172e22a3b
matthew110703 <unknownloop11@gmail.com>
2025-10-26 18:05:09 +0530

    Enahnced responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { Button, Divider } from "@/components/ui";
 
 const Home = () => {
   return (
-    <div className="space-y-24 xl:space-y-32">
+    <div className="space-y-20 xl:space-y-32">
       <Hero />
       <Portfolio />
       <AboutStats />
       <Domains />
-      <div className="flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="flex flex-col items-center justify-center gap-12 px-4 py-8 lg:py-16">
         <h2 className="text-center text-3xl lg:text-6xl">
           You’ve got questions, we’ve got answers...{" "}
           <br className="block lg:hidden" /> Let’s Chat!
